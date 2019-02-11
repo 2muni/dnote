43d0a02ad8d6857662732f77a67efe20620ac102
2muni <relesinc@gmail.com>
2019-02-11 20:36:17 +0900

    migrate Virtual env && Authentication

---
+++ backend/dnote/dnote/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+
+from notes import urls
+from django.conf.urls import include, url
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    url(r"^api/", include(urls)),
+    url(r"^api/auth", include("knox.urls")),
+]
--- backend/notes/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Notes
-
-
-class NoteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Notes
-        fields = ("id", "text")
